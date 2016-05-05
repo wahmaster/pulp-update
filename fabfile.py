6e23e5b04e410004678110d549e5c29c93dee85a
Role user managed by puppet <fabric@bolt-pr-1.build.internal>
2016-05-05 19:06:28 +0000

    Added the rest of the pulp repos

--- fabfile.py
+++ fabfile.py
@@ -12,14 +12,75 @@ from fabric.colors import red, green
 import re
 # import os
 
-env.skip_bad_hosts = True
-env.abort_on_prompts = True
-env.warn_only = True
-env.user = 'fabric'
-env.key_filename = '/opt/fabric/.ssh/Monkey'
-env.ssh_config_path = '/opt/fabric/ssh_config'
-
-def centos6():
+def centos6Updates():
 	"""Does a pulp-admin sync of centos6_updates"""
 	sudo('pulp-admin rpm repo sync run --repo-id=centos6_updates')
 
+def puppetlabsProductsStage():
+	"""Does a pulp-admin sync of puppetlabs products EL6 stage"""
+	sudo('pulp-admin rpm repo sync run --repo-id=puppetlabs-products-EL6-stage')
+
+def puppetlabsDependenciesStage():
+	"""Does a pulp-admin sync of puppetlabs dependencies EL6 stage"""
+	sudo('pulp-admin rpm repo sync run --repo-id=puppetlabs-dependencies-EL6-stage')
+
+def epel6():
+	"""Does a pulp-admin sync of epel6"""
+	sudo('pulp-admin rpm repo sync run --repo-id=epel6')
+
+def newrelic():
+	"""Does a pulp-admin sync of newrelic"""
+	sudo('pulp-admin rpm repo sync run --repo-id=newrelic')
+
+def epel7():
+	"""Does a pulp-admin sync of epel7"""
+	sudo('pulp-admin rpm repo sync run --repo-id=epel7')
+
+def puppetlabsProductsEL7():
+	"""Does a pulp-admin sync of puppetlabs products EL7"""
+	sudo('pulp-admin rpm repo sync run --repo-id=puppetlabs-products-EL7')
+
+def puppetlabsDependenciesEL7():
+	"""Does a pulp-admin sync of puppetlabs dependencies EL7"""
+	sudo('pulp-admin rpm repo sync run --repo-id=puppetlabs-dependencies-EL7')
+
+def sensuEL6():
+	"""Does a pulp-admin sync of sensu EL6"""
+	sudo('pulp-admin rpm repo sync run --repo-id=sensu_el_6')
+
+def datastax():
+	"""Does a pulp-admin sync of datastax"""
+	sudo('pulp-admin rpm repo sync run --repo-id=datastax')
+
+def puppetlabsProductsEL6():
+	"""Does a pulp-admin sync of puppetlabs products EL6"""
+	sudo('pulp-admin rpm repo sync run --repo-id=puppetlabs-products-EL6')
+
+def puppetlabsDependenciesEL6():
+	"""Does a pulp-admin sync of puppetlabs dependencies EL6"""
+	sudo('pulp-admin rpm repo sync run --repo-id=puppetlabs-dependencies-EL6')
+
+def centos6():
+	"""Does a pulp-admin sync of centos6"""
+	sudo('pulp-admin rpm repo sync run --repo-id=centos6')
+
+def remiEL6():
+	"""Does a pulp-admin sync of remi EL5"""
+	sudo('pulp-admin rpm repo sync run --repo-id=remi-el6')
+
+def pulpUpdatAll():
+	"""Does a pulp-admin sync of everything"""
+	centos6Updates()
+	puppetlabsProductsStage()
+	puppetlabsDependenciesStage()
+	epel6()
+	newrelic()
+	epel7()
+	puppetlabsProductsEL7()
+	puppetlabsDependenciesEL7()
+	sensuEL6()
+	datastax()
+	puppetlabsProductsEL6()
+	puppetlabsDependenciesEL6()
+	centos6()
+	remiEL6()
